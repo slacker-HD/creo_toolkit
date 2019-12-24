43f791afadde8b77299f5c7a5673c03f4765fa32
slacker_HD <hudi@linuxmail.org>
2019-12-24 14:12:49 +0800

    fix error

--- CreoAccess/CreoAccess/stdafx.h
+++ CreoAccess/CreoAccess/stdafx.h
@@ -37,7 +37,7 @@
 #include <afxcmn.h>                     // MFC 对 Windows 公共控件的支持
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-#import "C:\Program Files\Common Files\System\ado\msado15.dll" no_namespace  rename("EOF", "adoEOF")
+#import "C:\\Program Files\\Common Files\\System\\ado\\msado15.dll" no_namespace  rename("EOF", "adoEOF")
 #if !defined CATCH_ERROR
 #define CATCH_ERROR											\
 		{													\
