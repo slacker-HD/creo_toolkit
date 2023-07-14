62777bc91239e1183605282a5b0792d1d38cef46
slacker_HD <hudi@linuxmail.org>
2023-07-14 12:08:23 +0800

    优化代码

--- AsyncProjectWithPdcurses/src/Test.c
+++ AsyncProjectWithPdcurses/src/Test.c
@@ -13,24 +13,21 @@ void initialize()
 	curs_set(0);
 }
 
-void shutdown()
-{
-	endwin();
-}
-
 int main(int argc, char *argv[])
 {
 
 	initialize();
-	mvprintw(LINES / 2, (COLS - (int)strlen("启动会话中...")) / 2, "启动会话中...");
+	mvprintw(LINES / 2, (COLS - (int)strlen("启动CREO会话")) / 2, "启动CREO会话");
 	refresh();
 	ProEngineerStart("C:\\PTC\\Creo 2.0\\Parametric\\bin\\parametric.exe", "");
-	mvprintw(LINES / 2, (COLS - (int)strlen("会话已启动，按任意键关闭会话并退出本程序...")) / 2, "会话已启动，按任意键关闭会话并退出本程序...");
+	mvprintw(LINES / 2 + 1, (COLS - (int)strlen("会话已启动，按任意键关闭会话")) / 2, "会话已启动，按任意键关闭会话");
 	refresh();
 	getch();
-	mvprintw(LINES / 2, (COLS - (int)strlen("结束会话。程序退出...")) / 2, "结束会话。程序退出...");
-	refresh();
 	ProEngineerEnd();
-	shutdown();
+	mvprintw(LINES / 2 + 3, (COLS - (int)strlen("会话结束。按任意键退出 ")) / 2, "会话结束。按任意键退出 ");
+	refresh();
+	getch();
+	endwin();
 	return 0;
 }
+
