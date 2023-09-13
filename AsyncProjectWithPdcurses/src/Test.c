69eb5fbd08104002a04e14cd409a582a84c17b22
slacker_HD <hudi@linuxmail.org>
2023-09-13 10:48:04 +0800

    修改PDcurses示例代码添加颜色系统以演示。

--- AsyncProjectWithPdcurses/src/Test.c
+++ AsyncProjectWithPdcurses/src/Test.c
@@ -3,10 +3,18 @@
 #include "./includes/curspriv.h"
 #include "./includes/panel.h"
 
+void initcolor()
+{
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_CYAN, COLOR_YELLOW);
+	init_pair(3, COLOR_RED, COLOR_GREEN);
+}
+
 void initialize()
 {
 	initscr();
 	start_color();
+	initcolor();
 	raw();
 	cbreak();
 	noecho();
@@ -15,19 +23,20 @@ void initialize()
 
 int main(int argc, char *argv[])
 {
-
 	initialize();
+	attron(COLOR_PAIR(1));
 	mvprintw(LINES / 2, (COLS - (int)strlen("启动CREO会话\xE4\xB8\xad...")) / 2, "启动CREO会话\xE4\xB8\xad...");
 	refresh();
 	ProEngineerStart("C:\\PTC\\Creo 2.0\\Parametric\\bin\\parametric.exe", "");
+	attron(COLOR_PAIR(2));
 	mvprintw(LINES / 2 + 1, (COLS - (int)strlen("会话已启动，按任意键关闭会话")) / 2, "会话已启动，按任意键关闭会话");
 	refresh();
 	getch();
 	ProEngineerEnd();
-	mvprintw(LINES / 2 + 3, (COLS - (int)strlen("会话结束。按任意键退出 ")) / 2, "会话结束。按任意键退出 ");
+	attron(COLOR_PAIR(3));
+	mvprintw(LINES / 2 + 3, (COLS - (int)strlen("会话结束。按任意键退\xE5\x87\xBA...")) / 2, "会话结束。按任意键退\xE5\x87\xBA...");
 	refresh();
 	getch();
 	endwin();
 	return 0;
 }
-
