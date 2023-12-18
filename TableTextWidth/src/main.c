99984b4be33f226e8d8531f5882c9ad55d6ae9ed
slacker_HD <hudi@linuxmail.org>
2023-12-18 13:41:58 +0800

    完成ableTextWidth

--- TableTextWidth/src/main.c
+++ TableTextWidth/src/main.c
@@ -31,13 +31,47 @@ static uiCmdAccessState AccessDRW(uiCmdAccessMode access_mode)
         return ACCESS_INVISIBLE;
 }
 
-void FormatText()
+void WrapText()
 {
     ProError status;
     ProSelection *tableSels = NULL;
     int size;
     int table_segment, row, column;
-    double columnwidth;
+    ProDwgtable table;
+
+    status = ProMessageDisplay(MSGFILE, "IMI_PrompSelectTableCell");
+    status = ProSelect((char *)"table_cell", 1, NULL, NULL, NULL, NULL, &tableSels, &size);
+    if (status != PRO_TK_NO_ERROR || size < 1)
+    {
+        return;
+    }
+
+    status = ProArraySizeGet(tableSels, &size);
+    if (status != PRO_TK_NO_ERROR || size <= 0)
+    {
+        return;
+    }
+    status = ProSelectionDwgtblcellGet(tableSels[0], &table_segment, &row, &column);
+    if (status != PRO_TK_NO_ERROR)
+    {
+        return;
+    }
+    status = ProSelectionDwgtableGet(tableSels[0], &table);
+    if (status != PRO_TK_NO_ERROR)
+    {
+        return;
+    }
+    status = ProDwgtableCelltextWrap(&table, row + 1, column + 1);
+    status = ProMacroLoad(L"~ Command `ProCmdDwgRegenModel` ;~ Command `ProCmdWinActivate`;");
+}
+
+void setTextWidth(double scale)
+{
+    ProError status;
+    ProSelection *tableSels = NULL;
+    int size;
+    int table_segment, row, column;
+    double width;
     ProDwgtable table;
     ProDtlnote note;
     ProTextStyle textStyle;
@@ -66,102 +100,39 @@ void FormatText()
         return;
     }
 
-    status = ProDwgtableColumnSizeGet(&table, table_segment, column, &columnwidth);
     status = ProDwgtableCellNoteGet(&table, column + 1, row + 1, &note);
     status = ProNoteTextStyleGet(&note, &textStyle);
-    // 根据字体计算宽度因子
 
-    status = ProTextStyleWidthSet(textStyle, 0.6);
+    status = ProTextStyleWidthGet(textStyle, &width);
+    status = ProTextStyleWidthSet(textStyle, width + scale);
     status = ProNoteTextStyleSet(&note, textStyle);
     status = ProTextStyleFree(&textStyle);
-
-    status = ProDwgtableCelltextWrap(&table, row + 1, column + 1);
     status = ProMacroLoad(L"~ Command `ProCmdDwgRegenModel` ;~ Command `ProCmdWinActivate`;");
 }
 
-// void FormatText()
-// {
-//     ProError status;
-//     ProSelection *tableSels = NULL;
-//     int size;
-//     int table_segment, row, column;
-//     double columnwidth;
-//     ProDwgtable table;
-//     ProDtlnote note;
-//     ProDtlnotedata notedata;
-//     ProDtlnoteline *p_lines;
-//     ProDtlnotetext *p_texts;
-//     int n_lines, n_texts;
-//     double textwidth;
-
-//     status = ProMessageDisplay(MSGFILE, "IMI_PrompSelectTableCell");
-//     status = ProSelect((char *)"table_cell", 1, NULL, NULL, NULL, NULL, &tableSels, &size);
-//     if (status != PRO_TK_NO_ERROR || size < 1)
-//     {
-//         return;
-//     }
-
-//     status = ProArraySizeGet(tableSels, &size);
-//     if (status != PRO_TK_NO_ERROR || size <= 0)
-//     {
-//         return;
-//     }
-//     status = ProSelectionDwgtblcellGet(tableSels[0], &table_segment, &row, &column);
-//     if (status != PRO_TK_NO_ERROR)
-//     {
-//         return;
-//     }
-//     status = ProSelectionDwgtableGet(tableSels[0], &table);
-//     if (status != PRO_TK_NO_ERROR)
-//     {
-//         return;
-//     }
-
-//     status = ProDwgtableColumnSizeGet(&table, table_segment, column, &columnwidth);
-//     status = ProDwgtableCellNoteGet(&table, column + 1, row + 1, &note);
-//     status = ProDtlnoteDataGet(&note, NULL, PRODISPMODE_NUMERIC, &notedata);
-//     status = ProDtlnotedataLinesCollect(notedata, &p_lines);
-
-//     if (status == PRO_TK_NO_ERROR)
-//     {
-//         status = ProArraySizeGet((ProArray)p_lines, &n_lines);
-//         status = ProDtlnotelineTextsCollect(p_lines[0], &p_texts);
-
-//         status = ProArraySizeGet((ProArray)p_lines, &n_texts);
-//         if (status == PRO_TK_NO_ERROR)
-//         {
-//             status = ProDtlnotetextWidthGet(p_texts[0], &textwidth);
-//             // if (columnwidth < CellText.GetLength() * 2.753)
-//             // {
-//             status = ProDtlnotetextWidthSet(p_texts[0], -1.0); // columnwidth/CellText.GetLength()*0.85/2.753);
-//             status = ProDtlnotetextHeightSet(p_texts[0], 5.5); // columnwidth/CellText.GetLength()*0.85/2.753);
-
-//             // }
-//             // 3.5的字符，100宽度为3.24每个字符
-//             //
-//             status = ProDtlnotelineTextsSet(p_lines[0], p_texts);
-
-//             // status = ProArrayFree((ProArray *)&p_texts);
-//         }
-//         status = ProDtlnoteldataLinesSet(notedata, p_lines);
-
-//         ProTextStyleWidthSet
-//         // status = ProDtlnotedataFree(notedata);
-//     }
-//     // status = ProArrayFree((ProArray *)&p_lines);
-
-//     ProTestDtlNoteTextModify(notedata);
-// }
-
+void IncreaseWidth()
+{
+    setTextWidth(0.05);
+}
+void DecreaseWidth()
+{
+    setTextWidth(-0.05);
+}
 int user_initialize()
 {
     ProError status;
-    uiCmdCmdId IMI_FormatTextmenuID;
+    uiCmdCmdId IMI_TextWrapmenuID, IMI_TextWidthIncmenuID, IMI_TextWidthDecmenuID;
 
     status = ProMenubarMenuAdd("IMI_TableTextWidthmenu", "IMI_TableTextWidthmenu", "About", PRO_B_TRUE, MSGFILE);
 
-    status = ProCmdActionAdd("IMI_UnitInterPret_Act", (uiCmdCmdActFn)FormatText, uiProeImmediate, AccessDRW, PRO_B_TRUE, PRO_B_TRUE, &IMI_FormatTextmenuID);
-    status = ProMenubarmenuPushbuttonAdd("IMI_TableTextWidthmenu", "IMI_FormatTextmenu", "IMI_FormatTextmenu", "IMI_FormatTextmenutips", NULL, PRO_B_TRUE, IMI_FormatTextmenuID, MSGFILE);
+    status = ProCmdActionAdd("IMI_TextWrap_Act", (uiCmdCmdActFn)WrapText, uiProeImmediate, AccessDRW, PRO_B_TRUE, PRO_B_TRUE, &IMI_TextWrapmenuID);
+    status = ProMenubarmenuPushbuttonAdd("IMI_TableTextWidthmenu", "IMI_TextWrapmenu", "IMI_TextWrapmenu", "IMI_TextWrapmenutips", NULL, PRO_B_TRUE, IMI_TextWrapmenuID, MSGFILE);
+
+    status = ProCmdActionAdd("IMI_TextWidthInc_Act", (uiCmdCmdActFn)IncreaseWidth, uiProeImmediate, AccessDRW, PRO_B_TRUE, PRO_B_TRUE, &IMI_TextWidthIncmenuID);
+    status = ProMenubarmenuPushbuttonAdd("IMI_TableTextWidthmenu", "IMI_TextWidthIncmenu", "IMI_TextWidthIncmenu", "IMI_TextWidthIncmenutips", NULL, PRO_B_TRUE, IMI_TextWidthIncmenuID, MSGFILE);
+
+    status = ProCmdActionAdd("IMI_TextWidthDec_Act", (uiCmdCmdActFn)DecreaseWidth, uiProeImmediate, AccessDRW, PRO_B_TRUE, PRO_B_TRUE, &IMI_TextWidthDecmenuID);
+    status = ProMenubarmenuPushbuttonAdd("IMI_TableTextWidthmenu", "IMI_TextWidthDecmenu", "IMI_TextWidthDecmenu", "IMI_TextWidthDecmenutips", NULL, PRO_B_TRUE, IMI_TextWidthDecmenuID, MSGFILE);
 
     return PRO_TK_NO_ERROR;
 }
